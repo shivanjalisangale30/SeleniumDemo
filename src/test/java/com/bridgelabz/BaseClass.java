cb86ffd67e7180527e58b95f899ac153e51dd3b7
shivanjalisangale30 <shivanjalisangale5456>
2020-01-11 15:32:12 +0530

    added test case for xpath locators

--- src/test/java/com/bridgelabz/BaseClass.java
+++ src/test/java/com/bridgelabz/BaseClass.java
@@ -2,16 +2,41 @@ package com.bridgelabz;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
+
+
 
 public class BaseClass {
 
+    enum DriverType {CHROMEDRIVER, MOZILLA_FIREFOX}
+
+    ;
     public WebDriver driver;
+
+
+//    public String userChoice() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Select the driver\n1.ChormeDriver\n2.Mozilla Firefox");
+//        String choice = sc.nextLine();
+//        return choice;
+//    }
+
+//    @BeforeMethod
+//    @Parameters("browser")
+//    public void setUp(String browser) {
+//        if (browser.equalsIgnoreCase("chrome")) {
+//            System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
+//            driver = new ChromeDriver();
+//        }
+//        else if (browser.equalsIgnoreCase("firefox")) {
+//            System.setProperty("webdriver.geckodriver.driver", "./Drivers/firefox-geckodriver_72.0.1+build1-0ubuntu0.18.04.1_amd64-1.deb");
+//            driver = new FirefoxDriver();
+//        }
+//    }
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
         driver = new ChromeDriver();
     }
 
--- src/test/java/com/bridgelabz/FacebookSeleniumTest.java
+++ src/test/java/com/bridgelabz/FacebookSeleniumTest.java
@@ -10,24 +10,29 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
-public class DemoExamples extends BaseClass {
+
+public class FacebookSeleniumTest extends BaseClass {
+
+    String facebookURL = "https://www.facebook.com";
+
     @Test
-    public void CaptureScreenshot_ActiTIMEPage() throws IOException {
+    public void CaptureScreenshot_ActiTIMEPage() throws IOException{
         Date date = new Date();
         String date1 = date.toString();
         String date2 = date.toString().replaceAll(":", "-");
         System.out.println("Date:" + date1);
         System.out.println("Replacement" + date2);
-        driver.get("https://fundoopush-dev.bridgelabz.com/login");
+        driver.get(facebookURL);
         TakesScreenshot screenshot = (TakesScreenshot) driver;
         File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
-        File desFile = new File("/home/admin1/Desktop/Selenium/ScreenShots/" + date2 + "LoginScreenshot.jpg");
+        String imageName = new Object() {}.getClass().getEnclosingMethod().getName();
+        File desFile = new File("/home/admin1/Desktop/SeleniumDemo/Screenshotes/" + date2 + imageName + ".jpg");
         FileUtils.copyFile(srcFile, desFile);
     }
 
     @Test
     public void browserNavigationExample() throws InterruptedException {
-        driver.get("https://fundoopush-dev.bridgelabz.com/login");
+        driver.get(facebookURL);
         driver.navigate().to("https://www.gmail.com");
         Thread.sleep(1000);
         driver.navigate().forward();
@@ -39,7 +44,7 @@ public class DemoExamples extends BaseClass {
 
     @Test
     public void upcastingToWebDriver_LaunchBrowser() throws InterruptedException {
-        driver.get("https://www.google.com");
+        driver.get(facebookURL);
         String title = driver.getTitle();
         System.out.println("Title is " + title);
         String currentUrl = driver.getCurrentUrl();
@@ -51,7 +56,7 @@ public class DemoExamples extends BaseClass {
 
     @Test
     public void Keyboard_Mouse_Operations() throws InterruptedException, AWTException {
-        driver.navigate().to("https://fundoopush-dev.bridgelabz.com/login");
+        driver.navigate().to(facebookURL);
         Thread.sleep(5000);
         Robot robot = new Robot();
         robot.mouseMove(300, 500);
@@ -68,7 +73,7 @@ public class DemoExamples extends BaseClass {
 
     @Test
     public void LocatorsExample_ById() throws InterruptedException {
-        driver.get("https://www.facebook.com");
+        driver.get(facebookURL);
         WebElement user = driver.findElement(By.id("email"));
         user.clear();
         user.sendKeys("shivanjali");
@@ -82,15 +87,56 @@ public class DemoExamples extends BaseClass {
 
     @Test
     public void LocatorsExample_ByName() throws InterruptedException {
-        driver.get("https://www.facebook.com");
-        WebElement user = driver.findElement(By.name(""));
+        driver.get(facebookURL);
+        WebElement user = driver.findElement(By.name("email"));
         user.clear();
         user.sendKeys("shivanjali");
         WebElement pass = driver.findElement(By.name("pass"));
         pass.clear();
         Thread.sleep(2000);
         pass.sendKeys("147859623");
-        driver.findElement(By.id("loginbutton")).click();
+        driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
         Thread.sleep(2000);
     }
+
+
+    @Test
+    public void LocatorsExample_Bycssselector() throws InterruptedException {
+        driver.get(facebookURL);
+        WebElement user = driver.findElement(By.cssSelector(" #email"));
+        user.clear();
+        user.sendKeys("shivanjali");
+        WebElement pass = driver.findElement(By.name("pass"));
+        pass.clear();
+        Thread.sleep(2000);
+        pass.sendKeys("147859623");
+        driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void LocatorsExample_Byxpath_TextAndContains() throws InterruptedException {
+        driver.get(facebookURL);
+        //xPath for user
+        String xpUser = "//input[@id='email']";
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(xpUser)).sendKeys("9869191658");
+        Thread.sleep(2000);
+        //xPath for pwd
+        String xpPWD = "//input[@id='pass']";
+        driver.findElement(By.xpath(xpPWD)).sendKeys("sonu&12345");
+        Thread.sleep(2000);
+        //xPath for image
+        String xpImage = "//div[@class='_5iyy']//img[@class='img']";
+        WebElement image = driver.findElement(By.xpath(xpImage));
+        String widthImage = image.getAttribute("width");
+        System.out.println("Width of the image is " + widthImage);
+        String heightImage = image.getAttribute("height");
+        System.out.println("Height of the image is " + heightImage);
+        //xPath for login
+        String xpLogin = "//input[@id='u_0_b']";
+        driver.findElement(By.xpath(xpLogin)).click();
+        Thread.sleep(2000);
+    }
+
 }
